d58567c2cca64165caeb710294f4a586c368dc01
Victor Simonin <victor.simonin@epita.fr>
2022-06-16 18:20:27 +0200

    [Add] Refacto ex3 part7 simulation

--- Exercice3/simulation.py
+++ Exercice3/simulation.py
@@ -2,22 +2,20 @@ import numpy as np
 import pandas as pd
 from numpy.random import normal
 
+# Here we define our main variable 
 sigma2 = 3
 n = 1000000
 d = 3
 X = [np.random.randint(10000, size=n)] * d
-noise = normal(0,size=n, scale=np.sqrt(sigma2))
+noise = normal(0, size=n, scale=np.sqrt(sigma2))
 
 ols_y = 12.34*X[0] + 3.67*X[1] + 4.92*X[2] + 69.2
 real_y = ols_y + noise
 
-def noise_variance_estimator(real_y, y):
-    res = 0
-    for i in range(len(real_y)):
-        res += (real_y[i] - y[i])**2
-    return res/(n - (d+1))
+noised_var = 0
+for i in range(len(ols_y)):
+    noised_var += (ols_y[i] - real_y[i])**2
+noised_var = noised_var/(n - (d+1))
 
-res = noise_variance_estimator(ols_y, real_y)
-
-print("The approximated noise variance is:",res)
+print("The approximated noise variance is:", noised_var)
 print("The real variance is:", sigma2)
